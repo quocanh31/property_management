29d7506dde499990961b56581904b5dbfaeb50b6
Mai Huong <huong.nguyenmai158@gmail.com>
2019-08-13 09:23:40 +0700

    fixed login

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import pdfkit
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/property_manage"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/property_management"
 SECRET_KEY = 'UHx24EQz8R8lDw2ENCcDvHyRZGqyEANu'
 config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
